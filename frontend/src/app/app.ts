d38b9e1c954c9db839a0066c42f99089d223bd8c
luckyZharkyn <lucky_zharkyn@mail.ru>
2026-09-23 14:50:36 +0500

    commit 2

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeader } from './features/scenario/components/app-header/app-header';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [AppHeader, RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
-export class App {
-  protected readonly title = signal('frontend');
-}
+export class App {}
